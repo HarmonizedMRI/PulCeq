ebfa5161eae171f94834818493b889fc99483a2f
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2023-06-01 09:43:30 -0400

    Initial commit On branch dev 	modified:   block.h 	new file:   constants.h

--- src/block.h
+++ src/block.h
@@ -2,59 +2,55 @@
 #define BLOCK_H
 
 typedef struct {
-    short nSamples;    /* number of 4us waveform samples */
-    short* magnitude;  
+    short nSamples;      /* number of waveform samples */
+    float* magnitude;  
     short* phase;     
-} PULSEQ_SHAPE_ARBITRARY;
+} PulseqShapeArbitrary;
     
 typedef struct {
     short riseTime;    /* us */
     short flatTime;    /* us */
     short fallTime;    /* us */
-} PULSEQ_SHAPE_TRAP;
-
-/*
-typedef union {
-    PULSEQ_SHAPE_ARBITRARY wav;
-    PULSEQ_SHAPE_TRAP trap;
-} PULSEQ_SHAPE;
-*/
+} PulseqShapeTrap;
 
 typedef struct {
     short type;                   /* NULL or ARBITRARY */
     short delay;                  /* us */
     float amplitude;              /* Gauss */
-    PULSEQ_SHAPE_ARBITRARY wav;   /* arbitrary waveform in hardware units */
-} PULSEQ_RF;
+    PulseqShapeArbitrary wav;   /* arbitrary waveform in hardware units */
+} PulseqRF;
 
 typedef struct {
     short type;          /* NULL, TRAP, or ARBITRARY */
     short delay;         /* us */
     float amplitude;     /* Gauss/cm */
     union {
-        PULSEQ_SHAPE_ARBITRARY wav;
-        PULSEQ_SHAPE_TRAP trap;
+        PulseqShapeArbitrary wav;
+        PulseqShapeTrap trap;
     } shape;
-} PULSEQ_GRAD;
+} PulseqGrad;
 
 typedef struct {
-    short type;         /* NULL or ADC */
+    int   type;         /* NULL or ADC */
     short numSamples;   /* number of ADC samples */
     short dwell;        /* us. Must be multiple of 2us */
     short delay;        /* us */
-} PULSEQ_ADC;
+} PulseqADC;
 
 typedef struct {
-    short ID;
-    short duration;
-    PULSEQ_RF   rf;
-    PULSEQ_GRAD gx;
-    PULSEQ_GRAD gy;
-    PULSEQ_GRAD gz;
-    PULSEQ_ADC  adc;
-} PULSEQ_BLOCK; 
+    float      duration;     /* sec */
+    PulseqRF   rf;
+    PulseqGrad gx;
+    PulseqGrad gy;
+    PulseqGrad gz;
+    PulseqADC  adc;
+    PulseqTrig trig;
+    int ID;                 /* block ID (optional) */
+    int* v1;                /* to be defined as needed by user (optional) */
+    float * v2;             /* to be defined as needed by user (optional) */
+} PulseqBlock; 
 
-int read_block(FILE* fid, PULSEQ_BLOCK* blk);  /* cf. pulsegeq.readblock() */
-int read_grad(FILE* fid, PULSEQ_GRAD* grad);   /* cf. pulsegeq.readgrad() */
+int read_block(FILE* fid, PulseqBlock* blk);  /* cf. pulsegeq.readblock() */
+int read_grad(FILE* fid, PulseqGrad* grad);   /* cf. pulsegeq.readgrad() */
 
 #endif
---
+++ src/constants.h
@@ -0,0 +1,17 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+/* It will hopefully be possible to use 'typedef enum' definitions 
+   here in future EPIC releases */
+
+/* gradient/RF types */
+#define TRAP 1
+#define ARBITRARY 2
+
+/* Block types */
+#define DELAY 0
+#define ADC 1
+#define RF    2
+#define GRAD  3
+
+#endif
