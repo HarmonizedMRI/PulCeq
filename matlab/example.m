3b2c092a16f74db3bcfc3762cec2ded3f2aa7882
Jon-Fredrik Nielsen <jfnielsen@gmail.com>
2023-07-11 21:32:21 -0400

    On branch dev 	modified:   example.m

--- matlab/example.m
+++ matlab/example.m
@@ -1,23 +1,27 @@
 % create a .seq file
-addpath ~/github/HarmonizedMRI/MP-RAGE/sequence/Pulseq/
-% writeMPRAGE;
+addpath ~/github/HarmonizedMRI/B0shimming/sequence/Pulseq/
+writeB0;
+fn = 'b0';
 
 % convert to PulCeq representation
 addpath ~/github/HarmonizedMRI/PulCeq/matlab/ 
-%ceq = seq2ceq('mprage.seq');
+fprintf('Converting to PulCeq struct:\n')
+ceq = seq2ceq([fn '.seq'], 'verbose', false);
 
 % Write to TOPPE files
 addpath ~/github/toppeMRI/toppe/
 sysGE = toppe.systemspecs('maxGrad', 5, ...   % G/cm
-    'B0', 3.0, ...
     'maxSlew', 20, ...                % G/cm/ms
     'maxRF', 0.15, ...                % Gauss. Must be >= peak RF in sequence.
-    'timessi', 100, ...               % us
+    'maxSlice', 60, ...
     'rfDeadTime', 72, ...             % us
     'rfRingdownTime', 54, ...         % us
+    'psd_rf_wait', 148, ...           % UHP @ RX28 = 148us
+    'psd_grd_wait', 156, ...          % UHP @ RX28 = 156us
     'segmentRingdownTime', 104, ...   % us
     'adcDeadTime', 40);               % us
 
-ceq2ge(ceq, sysGE, 'mprage.tar', true);
+fprintf('Writing TOPPE files:\n');
+ceq2ge(ceq, sysGE, [fn '.tar'], true);
 
 % The file 'mprage.tar' can now be executed on a GE scanner
