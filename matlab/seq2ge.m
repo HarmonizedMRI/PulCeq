e3ec9df9e05c4f9dfb6469870c8ac08918f73f8f
Rex Fung <55724942+rextlfung@users.noreply.github.com>
2023-09-06 23:06:33 -0400

    Adding default sysGE for seq2ge() (#8)
    Incorporated default sysGE parameters to allow users to simply convert .seq files to play on GE scanners without explicitly defining sysGE

--- matlab/seq2ge.m
+++ matlab/seq2ge.m
@@ -1,7 +1,35 @@
-function seq2ge(seqarg, sysGE, ofname)
+function seq2ge(seqarg, ofname, varargin)
+% Convert a Pulseq file (http://pulseq.github.io/) to a .tar file for GE scanners
+%
+% Inputs
+%   seqarg = a seq object, or name of a .seq file
+%
+% Ouputs
+%   none
+%
+% Effect
+%   Writes a .tar file containing all the required scan files to run on GE scanners
+
+% Parse inputs
+% Read in system specs defined from .seq file
+seq = mr.Sequence(); seq.read(seqarg); sys = seq.sys;
+
+% Define sysGE
+arg.sysGE = toppe.systemspecs(...
+    'psd_rf_wait', 200,... % us
+    'psd_grd_wait', 200,... % us
+    'maxGrad', sys.maxGrad/sys.gamma*100,... % G/cm
+    'maxSlew', 1.01*sys.maxSlew/sys.gamma/10,... % G/cm/ms. Factor > 1 is fudge factor to avoid exceeding limit after interpolating to GE raster time.
+    'rfDeadTime', sys.rfDeadTime*1e6,... % us
+    'rfRingdownTime', sys.rfRingdownTime*1e6,... % us
+    'adcDeadTime', sys.adcDeadTime*1e6...  % us
+);
+
+% Use explicitly provided sysGE (function from MIRT toolbox)
+arg = vararg_pair(arg, varargin);
 
 % Convert the .seq file/object to the PulCeq representation
 ceq = seq2ceq(seqarg);
 
 % Write to TOPPE files
-ceq2ge(ceq, sysGE, ofname);
+ceq2ge(ceq, arg.sysGE, ofname);
