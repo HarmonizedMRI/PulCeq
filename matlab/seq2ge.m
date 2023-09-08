583c97558a167339e428a6ffbd8e28a051b6db4b
Rex Fung <rexfung@umich.edu>
2023-09-08 16:20:30 -0400

    Changed seq2ge() to require explicit input of sysGE

--- matlab/seq2ge.m
+++ matlab/seq2ge.m
@@ -1,35 +1,36 @@
-function seq2ge(seqarg, ofname, varargin)
+function seq2ge(seqarg, sysGE, ofname)
 % Convert a Pulseq file (http://pulseq.github.io/) to a .tar file for GE scanners
 %
 % Inputs
 %   seqarg = a seq object, or name of a .seq file
+%   sysGE = sys struct representing GE scanner parameters. Provide an empty array [] to trigger default behavior
+%   ofname = output file name
 %
-% Ouputs
+% Outputs
 %   none
 %
 % Effect
 %   Writes a .tar file containing all the required scan files to run on GE scanners
 
-% Parse inputs
-% Read in system specs defined from .seq file
-seq = mr.Sequence(); seq.read(seqarg); sys = seq.sys;
+% Default behavior for sysGE
+if isempty(sysGE)
+    % Read in system specs defined from .seq file
+    seq = mr.Sequence(); seq.read(seqarg); sys = seq.sys;
 
-% Define sysGE
-arg.sysGE = toppe.systemspecs(...
-    'psd_rf_wait', 200,... % us
-    'psd_grd_wait', 200,... % us
-    'maxGrad', sys.maxGrad/sys.gamma*100,... % G/cm
-    'maxSlew', 1.01*sys.maxSlew/sys.gamma/10,... % G/cm/ms. Factor > 1 is fudge factor to avoid exceeding limit after interpolating to GE raster time.
-    'rfDeadTime', sys.rfDeadTime*1e6,... % us
-    'rfRingdownTime', sys.rfRingdownTime*1e6,... % us
-    'adcDeadTime', sys.adcDeadTime*1e6...  % us
-);
-
-% Use explicitly provided sysGE (function from MIRT toolbox)
-arg = vararg_pair(arg, varargin);
+    % Define sysGE
+    sysGE = toppe.systemspecs(...
+        'psd_rf_wait', 200,... % us
+        'psd_grd_wait', 200,... % us
+        'maxGrad', sys.maxGrad/sys.gamma*100,... % G/cm
+        'maxSlew', 1.01*sys.maxSlew/sys.gamma/10,... % G/cm/ms. Factor > 1 is fudge factor to avoid exceeding limit after interpolating to GE raster time.
+        'rfDeadTime', sys.rfDeadTime*1e6,... % us
+        'rfRingdownTime', sys.rfRingdownTime*1e6,... % us
+        'adcDeadTime', sys.adcDeadTime*1e6...  % us
+    );
+end
 
 % Convert the .seq file/object to the PulCeq representation
 ceq = seq2ceq(seqarg);
 
 % Write to TOPPE files
-ceq2ge(ceq, arg.sysGE, ofname);
+ceq2ge(ceq, sysGE, ofname);
