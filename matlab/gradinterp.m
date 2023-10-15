8b403013da76929e0860d755c3678de7065be6e9
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2023-10-15 14:20:42 -0400

    Add test function On branch develop 	modified:   gradinterp.m

--- matlab/gradinterp.m
+++ matlab/gradinterp.m
@@ -1,9 +1,12 @@
 function wav = gradinterp(g, sysGE, varargin)
 % Interpolate gradient waveforms and convert to Gauss/cm
 % We assume that there are 3 types of gradients:
-% gradType = 1: arbitrary gradient specified on regular raster time. Assumed to start and end at zero ('first' and 'last' points). 
-% gradType = 2: extended trapezoid, specified on "corners" of shape. Assumed to start at zero if and only if delay > 0.
-% gradType = 3: trapezoid, starting and ending at 0
+% 1: arbitrary gradient specified on regular raster time. Assumed to start and end at zero ('first' and 'last' points). 
+% 2: extended trapezoid, specified on "corners" of shape. Assumed to start at zero if and only if delay > 0.
+% 3: trapezoid, starting and ending at 0
+%
+% To run test function, do:
+% >> gradinterp('test');
 %
 % Inputs
 %    g         struct    Pulseq gradient event
@@ -11,6 +14,11 @@ function wav = gradinterp(g, sysGE, varargin)
 % 
 % Output
 %    wav   [n 1]     Gradient waveform interpolated to 4us raster time, Gauss/cm
+
+if isstr(g)
+    sub_test();
+    return;
+end
 
 % parse input options
 arg.seqGradRasterTime = 10e-6;
@@ -37,37 +45,27 @@ else
         tt_rast=g.tt/arg.seqGradRasterTime+0.5;
         if all(abs(tt_rast-(1:length(tt_rast))')<1e-6)  % samples assumed to be on center of raster intervals
             % arbitrary gradient on a regular raster
-            gradType = 1;
-            areaIn = sum(g.waveform)*arg.seqGradRasterTime;
             g.first = 0;
             g.last = 0;
             wavtmp = [g.first g.waveform(:)' g.last];
             tttmp = g.delay + [0 g.tt(:)' g.tt(end)+arg.seqGradRasterTime/2];
         else
             % extended trapezoid: shape specified on "corners" of waveform
-            gradType = 2;
-            areaIn = sum( (g.waveform(1:(end-1)) + g.waveform(2:end))/2 .* diff(g.tt) );
             wavtmp = g.waveform(:)';
             tttmp = g.delay + g.tt(:)';
         end
     else
         % Convert trapezoid to arbitrary gradient
-        gradType = 3;
-        areaIn = [g.riseTime/2 + g.flatTime + g.fallTime/2] * g.amplitude;
-        if g.flatTime > 0
-            wavtmp = [0 1 1 0]*g.amplitude;       
-            tttmp = g.delay + [0 g.riseTime g.riseTime+g.flatTime g.riseTime+g.flatTime+g.fallTime ];
-        else
-            wavtmp = [0 1 0]*g.amplitude;       
-            tttmp = g.delay + [0 g.riseTime g.riseTime+g.fallTime ];
-        end
+        [tttmp, wavtmp] = trap2arb(g);
     end
 
+    % Add delay
     if g.delay > 0
         wavtmp = [0 wavtmp];
         tttmp = [0 tttmp];
     end
 
+    % interpolate (includes the delay)
     tge = raster/2 : raster : tttmp(end);
     tmp = interp1(tttmp, wavtmp, tge);
 
@@ -79,3 +77,59 @@ else
     % convert to Gauss/cm
     wav = tmp(:).' / gamma * 100; % Gauss/cm
 end
+
+return
+
+function sub_test()
+    sys = mr.opts();   % default settings
+    fov = 24e-2;       % m
+    deltak = 1/fov;
+    
+    % define two trapezoids: before and after wrapping in trap4ge
+    g = mr.makeTrapezoid('x', sys, 'Area', 1*deltak);
+    g2 = trap4ge(g, 20e-6, sys);  % ensure that sample points are on 20us boundary
+    g.delay = 40e-6;              % on 20us boundary
+    g2.delay = 40e-6;             % on 20us boundary
+
+    subplot(131); 
+    title('Trapezoid interpolated to 4us, before wrapping in trap4ge');
+    sub_test_doone(g);
+
+    subplot(132);
+    title('Trapezoid interpolated to 4us, after wrapping in trap4ge');
+    sub_test_doone(g2);
+
+    subplot(133);
+    title('Plotted together');
+    sub_test_doone(g);
+    sub_test_doone(g2);
+
+return
+
+function sub_test_doone(g)
+
+sysGE = toppe.systemspecs();   % default settings
+raster = sysGE.raster*1e-6;   % sec
+
+wav = gradinterp(g, sysGE);
+tt = (1:length(wav))*raster - raster/2;  % us
+
+% plot in units of Gauss/cm
+g.amplitude = g.amplitude / sysGE.gamma * 100;   % G/cm
+[ttin, wavin] = trap2arb(g);
+
+hold on;
+plot(ttin*1e3,wavin,'b-x'); 
+plot(tt*1e3,wav,'ro'); 
+xlabel('time (ms)');
+ylabel('Gauss/cm');
+
+areaIn = (g.riseTime/2 + g.flatTime + g.fallTime/2)*g.amplitude;
+areaOut = sum(wav) * raster;
+
+legend(sprintf('In: area = %.6f G/cm*us', areaIn*1e6) , ...
+       sprintf('Out: area = %.6f G/cm*us', areaOut*1e6));
+
+fprintf('Areas = %.5f (in), %.5f (out) G/cm*us\n', areaIn*1e6, areaOut*1e6);
+
+return
