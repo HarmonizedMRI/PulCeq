1a8ecc26a744510f29adf686ca1a53a734e73018
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2023-10-15 13:17:12 -0400

    Move grad interpolation code to gradinterp.m On branch develop 	modified:   ceq2ge.m

--- matlab/ceq2ge.m
+++ matlab/ceq2ge.m
@@ -69,79 +69,11 @@ for p = 1:ceq.nParentBlocks
     end
 
     % Interpolate gradient waveforms and convert to Gauss/cm
-    % We assume that there are only 3 types of gradients:
-    % gradType = 1: arbitrary gradient specified on regular raster time. Assumed to start and end at zero ('first' and 'last' points). 
-    % gradType = 2: extended trapezoid, specified on "corners" of shape. Assumed to start at zero if and only if delay > 0.
-    % gradType = 3: trapezoid, starting and ending at 0
     for ax = {'x','y','z'}
-
         g = b.(['g' ax{1}]);
-
         if ~isempty(g)
             isDelayBlock = false;
-
-            if strcmp(g.type, 'grad')
-                % Arbitrary gradient
-                % restore shape: if we had a
-                % trapezoid converted to shape we have to find
-                % the "corners" and we can eliminate internal
-                % samples on the straight segments
-                % but first we have to restore samples on the
-                % edges of the gradient raster intervals
-                % for that we need the first sample
-
-                % check if we have an extended trapezoid, or an arbitrary gradient on a regular raster
-                tt_rast=g.tt/arg.seqGradRasterTime+0.5;
-                if all(abs(tt_rast-(1:length(tt_rast))')<1e-6)  % samples assumed to be on center of raster intervals
-                    % arbitrary gradient on a regular raster
-                    gradType = 1;
-                    areaIn = sum(g.waveform)*arg.seqGradRasterTime;
-                    g.first = 0;
-                    g.last = 0;
-                    wavtmp = [g.first g.waveform(:)' g.last];
-                    tttmp = g.delay + [0 g.tt(:)' g.tt(end)+arg.seqGradRasterTime/2];
-                else
-                    % extended trapezoid: shape specified on "corners" of waveform
-                    gradType = 2;
-                    areaIn = sum( (g.waveform(1:(end-1)) + g.waveform(2:end))/2 .* diff(g.tt) );
-                    wavtmp = g.waveform(:)';
-                    tttmp = g.delay + g.tt(:)';
-                end
-            else
-                % Convert trapezoid to arbitrary gradient
-                gradType = 3;
-                areaIn = [g.riseTime/2 + g.flatTime + g.fallTime/2] * g.amplitude;
-                if g.flatTime > 0
-                    wavtmp = [0 1 1 0]*g.amplitude;       
-                    tttmp = g.delay + [0 g.riseTime g.riseTime+g.flatTime g.riseTime+g.flatTime+g.fallTime ];
-                else
-                    wavtmp = [0 1 0]*g.amplitude;       
-                    tttmp = g.delay + [0 g.riseTime g.riseTime+g.fallTime ];
-                end
-            end
-
-            if g.delay > 0
-                wavtmp = [0 wavtmp];
-                tttmp = [0 tttmp];
-            end
-
-            tge = raster/2 : raster : tttmp(end);
-            tmp = interp1(tttmp, wavtmp, tge);
-
-            if any(isnan(tmp))
-                msg = sprintf('NaN in gradient trapezoid waveform after interpolation (parent block %d)', p);
-                error(msg);
-            end
-
-            % scale to preserve area
-            % 10 mG for 10us = 4.3e-2 Hz-sec, so don't scale if area less than this
-            if areaIn > 4.3e-2 & arg.preserveArea %& any(gradType == [2,3]) 
-                areaOut = sum(tmp) * raster;
-                tmp = tmp*areaIn/areaOut;
-            end
-
-            % convert to Gauss/cm
-            grad.(ax{1}) = tmp(:).' / gamma * 100; % Gauss/cm
+            grad.(ax{1}) = gradinterp(g, sysGE, 'seqGradRasterTime', arg.seqGradRasterTime);
         end
     end
 
