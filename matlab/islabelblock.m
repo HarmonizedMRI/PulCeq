1358af86ab5921c712b8941f25cfdd8f074e801c
Jon-Fredrik Nielsen <jfnielsen@gmail.com>
2024-04-04 12:40:40 -0400

    On branch ignorelabels 	deleted:    islabelblock.m

--- matlab/islabelblock.m
+++
@@ -1,9 +0,0 @@
-function ilb = islabelblock(block)
-% function ilb = islabelblock(block)
-% 
-% Returns true if block only contains a label and no RF/ADC/gradient events 
-
-%ilb = isfield(block, 'label') & isempty(block.rf) & isempty(block.adc) & ...
-%ilb = isfield(block, 'label') & isempty(block.rf) ...
-%        & isempty(block.gx) & isempty(block.gy) & isempty(block.gz);
-ilb = block.blockDuration < 2*eps;
