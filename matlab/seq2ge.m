fdafa19a328be66ae2cac09ef5f478ada12247b4
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2024-11-10 10:59:12 -0500

    On branch tv7 	modified:   seq2ge.m

--- matlab/seq2ge.m
+++ matlab/seq2ge.m
@@ -1,41 +1,15 @@
-function seq2ge(seqarg, sysGE, ofname)
-% Convert a Pulseq file (http://pulseq.github.io/) to a .tar file for GE scanners
+function seq2ge(ifn, ofn)
+% Convert a Pulseq file (http://pulseq.github.io/) to a binary 
+% sequence representation in the Pulserver binary format.
+% For more information about Pulserver, see https://infn-mri.github.io/pulserver/
 %
 % Inputs
-%   seqarg = a seq object, or name of a .seq file
-%   sysGE = sys struct representing GE scanner parameters. Provide an empty array [] to trigger default behavior
-%   ofname = output file name
+%   ifn = Pulseq (.seq) input file name, e.g., 'gre.seq'
+%   ofn = binary output file name, e.g., 'gre.bin'
 %
-% Outputs
-%   none
-%
-% Effect
-%   Writes a .tar file containing all the required scan files to run on GE scanners
+% Example:
+%   seq2ge('gre2d.seq', 'gre2d.bin')
+%   Then put gre2d.bin on your GE scanner and run with the 'pge2' interpreter
 
-% Get Pulseq system struct
-fprintf('Getting Pulseq system struct from .seq file...')
-seq = mr.Sequence(); seq.read(seqarg); sys = seq.sys;
-fprintf(' done\n');
-
-% Default behavior for sysGE
-if isempty(sysGE)
-    % Read in system specs defined from .seq file
-    seq = mr.Sequence(); seq.read(seqarg); sys = seq.sys;
-
-    % Define sysGE
-    sysGE = toppe.systemspecs(...
-        'psd_rf_wait', 200,... % us
-        'psd_grd_wait', 200,... % us
-        'maxGrad', sys.maxGrad/sys.gamma*100,... % G/cm
-        'maxSlew', 1.01*sys.maxSlew/sys.gamma/10,... % G/cm/ms. Factor > 1 is fudge factor to avoid exceeding limit after interpolating to GE raster time.
-        'rfDeadTime', sys.rfDeadTime*1e6,... % us
-        'rfRingdownTime', sys.rfRingdownTime*1e6,... % us
-        'adcDeadTime', sys.adcDeadTime*1e6...  % us
-    );
-end
-
-% Convert the .seq file/object to the PulCeq representation
-ceq = seq2ceq(seqarg);
-
-% Write to TOPPE files
-ceq2ge(ceq, sysGE, ofname, 'seqGradRasterTime', sys.gradRasterTime);
+ceq = seq2ceq(ifn);
+writeceq(ceq, ofn);
