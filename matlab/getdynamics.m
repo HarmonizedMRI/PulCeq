42d1d50dfd7593e33b923a4e611bb78c803490a0
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2024-11-10 20:55:37 -0500

    C

--- matlab/getdynamics.m
+++ matlab/getdynamics.m
@@ -1,6 +1,8 @@
 function loop = getdynamics(block, segmentID, parentBlockID, parentBlock)
 % Return vector containing waveform amplitudes, RF/ADC phase, etc,
 % for a Pulseq block, in physical (Pulseq) units.
+%
+% Also return the gradient energy on each axis in (G/cm)^2*sec 
 
 % defaults
 rfamp = 0;
@@ -10,6 +12,11 @@ amp.gx = 0;
 amp.gy = 0;
 amp.gz = 0;
 recphs = 0;
+energy.gx = 0;
+energy.gy = 0;
+energy.gz = 0;
+
+GAM = 4257.6;   % Hz/Gauss
 
 if ~isempty(block.rf)
     rfamp = max(abs(block.rf.signal));
@@ -22,15 +29,12 @@ for ax = {'gx','gy','gz'}
     if ~isempty(g)
         if strcmp(g.type, 'trap')
             amp.(ax{1}) = g.amplitude;
+            energy.(ax{1}) = (g.amplitude/GAM/100)^2 / 3 * g.riseTime ...   % (G/cm)^2*sec
+                          + (g.amplitude/GAM/100)^2 * g.flatTime ... 
+                          + (g.amplitude/GAM/100)^2 / 3 * g.fallTime;
         else
             % need to check polarity (sign) with respect to parent block
-            mx = max(g.waveform);
-            mn = min(g.waveform);
-            pbmx = max(parentBlock.(ax{1}).waveform);
-            pbmn = min(parentBlock.(ax{1}).waveform);
             amp.(ax{1}) = max(abs(g.waveform));
-            % if xor(mx>abs(mn), pbmx>abs(pbmn))
-            % --DC
             if any(g.waveform.*parentBlock.(ax{1}).waveform < 0)
                 amp.(ax{1}) = -amp.(ax{1});
             end
@@ -42,5 +46,10 @@ if ~isempty(block.adc)
     recphs = block.adc.phaseOffset;
 end
 
-loop = [segmentID parentBlockID rfamp rfphs rffreq amp.gx amp.gy amp.gz recphs block.blockDuration];
+loop = [segmentID parentBlockID ...
+        rfamp rfphs rffreq ...
+        amp.gx amp.gy amp.gz ...
+        recphs ...
+        block.blockDuration ...
+        energy.gx energy.gy energy.gz];
     
