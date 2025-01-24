0fa6c439c907cb2d46e46266e6c33bb40d193a46
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2025-01-24 12:27:34 -0500

    On branch tv7 	modified:   main.m

--- matlab/main.m
+++ matlab/main.m
@@ -1,15 +1,11 @@
 system('git clone git@github.com:pulseq/pulseq.git');
 addpath pulseq/matlab
 
-if 0
-    addpath test
-    writetestsequence;   % test.seq
-    ceq = seq2ceq('test.seq');
-else
-    %ceq = seq2ceq('~/Downloads/se_30sli_2mm_R3_15dir_b1k.seq');
-    ceq = seq2ceq('~/Downloads/epidiff_3_shot_ref_2p0mm_30sli.seq');
-end
+% addpath test
+% writetestsequence;   % test.seq
 
+% Convert .seq file to a PulCeq (Ceq) object
+ceq = seq2ceq('test.seq');
 
 % write Ceq struct to file. This is what pge2 will load and run.
-writeceq(ceq, 'test.tar');
+writeceq(ceq, 'test.pge');
