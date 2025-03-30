133c668ffaf90e48c0940f5a1c4be394115b8881
Jon-Fredrik Nielsen <jfnielsen@gmail.com>
2025-03-30 14:38:21 -0400

    On branch tv7-checks 	modified:   validate.m

--- matlab/+pge2/validate.m
+++ matlab/+pge2/validate.m
@@ -1,6 +1,6 @@
 function ok = validate(ceq, sys)
 %
-% Check compatibility of a 'ceq' sequence object with the 'Pulseq on GE v2' interpreter.
+% Check compatibility of a PulCeq (Ceq) sequence object with the 'Pulseq on GE v2' interpreter.
 
 % Check parent block timing.
 % Parent blocks are 'virtual' (waveform amplitudes are arbitrary/normalized), so only check
@@ -17,8 +17,7 @@ end
 % Here we construct base (virtual) segments and check that:
 %  - gradients start/end on zero at the start/end of each segment, respectively
 %  - RF events, ADC events, and pure delay blocks are not too close together
-
-for i  = 1:ceq.nSegments      % we use 'i' to count segments here and in the EPIC code
+for i = 1:ceq.nSegments      % we use 'i' to count segments here and in the EPIC code
     try 
         pge2.constructvirtualsegment(ceq.segments(i).blockIDs, ceq.parentBlocks, sys);
     catch ME
@@ -26,8 +25,7 @@ for i  = 1:ceq.nSegments      % we use 'i' to count segments here and in the EPI
     end
 end
 
-
-% Check scan loop.
+% Check scan loop.  TODO
 % This is where waveform amplitudes are set, so for each segment instance we must check that:
 %  - RF amplitude does not exceed hardware limit
 %  - gradient amplitude on each axis does not exceed hardware limit
