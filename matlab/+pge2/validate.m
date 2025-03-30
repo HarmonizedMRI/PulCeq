76184fa894a7c64b56be69896959c25fc5cb7222
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2025-03-30 19:22:25 -0400

    On branch tv7-checks 	modified:   validate.m

--- matlab/+pge2/validate.m
+++ matlab/+pge2/validate.m
@@ -29,14 +29,37 @@ end
 %  - gradient amplitude on each axis does not exceed hardware limit
 %  - gradient slew rate on each axis does not exceed hardware limit
 %  - gradients are continuous across block boundaries
-a = input('Check scan loop? It might take a while. (y/n) ', "S");
-if ~strcmp(a, 'y') 
-    return;
-end
+%a = input('Check scan loop? It might take a while. (y/n) ', "S");
+%if ~strcmp(a, 'y') 
+%    return;
+%end
 
 n = 1;   % block (row) number
 while n < ceq.nMax
     i = ceq.loop(n, 1);   % segment id
+
+    for j = 1:ceq.segments(i).nBlocksInSegment
+        % rf amp
+        assert(ceq.loop(n,3)/sys.gamma < sys.b1_max + eps, ...
+            sprintf('segment %d, block %d: RF amp exceeds limit', i, j));
+
+        % gradient amp
+        gamp = ceq.loop(n, 6)/sys.gamma/100;
+        assert(gamp < sys.g_max + eps, ...
+            sprintf('segment %d, block %d: x gradient amp (%.3f) exceeds limit', i, j, gamp));
+        gamp = ceq.loop(n, 8)/sys.gamma/100;
+        assert(gamp < sys.g_max + eps, ...
+            sprintf('segment %d, block %d: y gradient amp (%.3f) exceeds limit', i, j, gamp));
+        gamp = ceq.loop(n, 10)/sys.gamma/100;
+        assert(gamp < sys.g_max + eps, ...
+            sprintf('segment %d, block %d: z gradient amp (%.3f) exceeds limit', i, j, gamp));
+
+        % gradient slew
+
+        n = n + 1;
+    end
+
+    % 
 end
 
 
