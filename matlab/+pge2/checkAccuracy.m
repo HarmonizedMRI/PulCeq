6dfece6a9898415239f19198365ad8b950d6bdc3
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2025-11-07 23:36:19 -0500

    WIP On branch tv7_dev 	modified:   checkAccuracy.m

--- matlab/+pge2/checkAccuracy.m
+++ matlab/+pge2/checkAccuracy.m
@@ -1,16 +1,18 @@
-function checkAccuracy(ceq, sysGE, xmlPath, seq)
+function checkAccuracy(ceq, sysGE, seq, xmlPath, plt)
 %
-% Check accuracy of WTools sequence output (.xml files)
-% against the original Pulseq sequence, and against the Ceq object.
+% Check accuracy of the Ceq sequence represenation
+% and/or the WTools simulator output (.xml files), 
+% against the original Pulseq sequence.
 
 % arg = vararg_pair(arg, varargin);   % in ../
 
 % Loop through segments
 
-n = 1;     % row counter in ceq.loop
-cnt = 1;   % segment instance counter
+n = 1;     % block (row) counter 
+cnt = 0;   % segment instance counter
 
-while n < ceq.nMax  & cnt < 4
+while n < ceq.nMax & cnt < 2
+    cnt = cnt + 1;
 
     % determine block range
     i = ceq.loop(n,1);  % segment index
@@ -20,20 +22,22 @@ while n < ceq.nMax  & cnt < 4
     % Pulseq waveforms
     w = seq.waveforms_and_times(false, [n1 n2]);
 
-    % pge2 waveforms (from WTools)
-    d = pge2.read_segment_xml(sprintf('%sscan.xml.%04d', xmlPath, cnt));
-
     % Ceq object waveforms
     L = ceq.loop(n1:n2, :);
     S = pge2.getsegmentinstance(ceq, i, sysGE, L, 'rotate', true, 'interpolate', true);
 
+    % pge2 waveforms (from WTools)
+    d = pge2.read_segment_xml(sprintf('%sscan.xml.%04d', xmlPath, cnt));
+
+    %gx = interp1(d(1).time(1:end-3)/1e6 - sysGE.segment_dead_time, d(1).value(1:end-3), ...
+    %    w{1}(1,:), 'linear', 'extrap');
+
     % plot x gradient
     figure; hold on;
     plot(w{1}(1,:), w{1}(2,:)/sysGE.gamma/100, 'b-');  % Pulseq
-    plot(d(1).time(1:end-3)/1e6, d(1).value(1:end-3), 'rx');
-    plot(S.gx.t, S.gx.signal, 'go');
+    plot(d(1).time(1:end-3)/1e6 - sysGE.segment_dead_time, d(1).value(1:end-3), 'rx-');
+    plot(S.gx.t - sysGE.segment_dead_time, S.gx.signal, 'go');
     title(sprintf('Segment instance %d', cnt));
 
     n = n2 + 1;
-    cnt = cnt + 1;
 end
