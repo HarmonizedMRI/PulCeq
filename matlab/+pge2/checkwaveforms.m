77d60e338d427f99d450195284925689d2f94b3c
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2025-11-09 10:43:51 -0500

    Almost there On branch tv7_dev 	modified:   checkwaveforms.m

--- matlab/+pge2/checkwaveforms.m
+++ matlab/+pge2/checkwaveforms.m
@@ -1,26 +1,42 @@
-function checkwaveforms(ceq, sysGE, seq, xmlPath, plt)
-% function checkwaveforms(ceq, sysGE, seq, xmlPath, [plt = false])
+function checkwaveforms(ceq, sysGE, seq, xmlPath, varargin)
+% function checkwaveforms(ceq, sysGE, seq, xmlPath, varargin)
 %
-% Check accuracy of the gradient and RF waveforms
-% (timing and shapes) in the Ceq sequence representation
-% and/or the scanner/WTools simulator output (.xml files), 
-% against the original Pulseq sequence.
+% Check agreement between scanner/WTools sequence output 
+% and the original Pulseq (.seq) object.
+%
+% Inputs:
+%   ceq       struct       Ceq sequence object, see seq2ceq.m
+%   sysGE     struct       System hardware info, see pge2.getsys()
+% 
 
-if nargin < 5
-    plt = false;
+arg.row = [];      % row in .seq file. If not specified, check the whole scan.
+arg.plot = true;   % plot every segment instance, continue on keyboard press
+
+arg = vararg_pair(arg, varargin);   % in ../
+
+if ~isempty(arg.row)
+    arg.plot = true;
+end
+
+if isempty(arg.row)
+    n = 1;     % block (row) counter 
+    doNextSegment = true;
+else
+    n = arg.row;  
+    doNextSegment = false;
 end
 
 % Loop over segments
-n = 1;     % block (row) counter 
 cnt = 0;   % segment instance counter
-
-while n < ceq.nMax & cnt < 2
+while n < ceq.nMax % & cnt < 2
     cnt = cnt + 1;
 
     % determine block range
     i = ceq.loop(n,1);  % segment index
     n1 = n;
     n2 = n - 1 + ceq.segments(i).nBlocksInSegment;
+
+    [n1 n2]
 
     % Pulseq waveforms
     w = seq.waveforms_and_times(true, [n1 n2]);
@@ -39,6 +55,8 @@ while n < ceq.nMax & cnt < 2
     for iax = 1:length(ax)
         % pge2 interpreter output (.xml files)
         tt.pge2 = d(iax).time(2:end-3)/1e6 - sysGE.segment_dead_time;     
+        %[~,I] = unique(tt.pge2);
+        %tt.pge2(I+1) = tt.pge2(I+1) + 1e-12;  % offset any duplicate time points (steps)
         g.pge2 = d(iax).value(2:end-3);
 
         % Pulseq (.seq) object
@@ -51,24 +69,28 @@ while n < ceq.nMax & cnt < 2
         g.ceq = S.(ax{iax}).signal;
 
         % max percent difference
-        e = 100*max(abs(g.seqip-g.pge2))/max(max(abs(g.seqip), 1e-3));
+        e = 100*max(abs(g.seqip-g.pge2))/max(max(abs(g.seqip), 1e-3))
 
-        % Due to interpolation/quantization error, difference isn't exactly zero
-        % even if the pge2 output matches the .seq file exactly as intended
-        tol = 3;  % so we don't worry about differences below tol
-        if e > tol   
+        if e > inf
             fprintf('Gradient waveform mismatch (row %d, segment %d, %s) -- exiting\n', n, i, ax{iax});
-            if plt
-                % plot gradient
-                figure; hold on;
-                title(sprintf('%s, segment instance %d', ax{iax}, cnt));
-                plot(tt.seq, g.seq, 'black-');   % Pulseq (ideal) waveform
-                plot(tt.pge2, g.pge2, 'rx');
-                %plot(tt.ceq, g.ceq, 'bo-');
-                %legend('pge2', 'ceq', 'seq');
-                legend('seq', 'pge2');
+            arg.plot = true;
+            doNextSegment = false;
+        end
+
+        if arg.plot
+            subplot(5,1,iax);
+            plot(tt.seq, g.seq, 'black-');   % Pulseq (ideal) waveform
+            hold on;
+            plot(tt.pge2, g.pge2, 'r.-');
+            %plot(tt.ceq, g.ceq, 'o');
+            hold off
+            %legend('pge2', 'ceq', 'seq');
+            legend('seq', 'pge2');
+            xlabel('time (sec)');
+            ylabel(sprintf('%s (G/cm)', ax{iax}), 'Rotation', 0);
+            if iax == 1
+                title(sprintf('segment starting at row %d (count = %d)', n, cnt));
             end
-            return;
         end
     end
 
@@ -81,43 +103,59 @@ while n < ceq.nMax & cnt < 2
     rf.seq = w{4}(2,:)/sysGE.gamma;    % Gauss
 
     % pge2 interpreter output
-    tt.pge2 = d(5).time(2:end-3)/1e6 - sysGE.segment_dead_time - sysGE.psd_rf_wait;
+    tt.rho = d(5).time(2:end-3)/1e6 - sysGE.segment_dead_time - sysGE.psd_rf_wait;
+    [~,I] = unique(tt.rho);
+    tt.rho(I+1) = tt.rho(I+1) + 1e-12;  % offset any duplicate time points (steps)
     rho = d(5).value(2:end-3);                     % a.u.
     rho = rho/max(abs(rho)) * max(abs(rf.seq));    % Gauss
 
     tt.theta = d(6).time(2:end-3)/1e6 - sysGE.segment_dead_time - sysGE.psd_rf_wait;
-    [tt.theta, I] = unique(tt.theta);
-    theta = d(6).value(2:end-3);
-    theta = theta/2^23 * pi;         % radians
-    theta = theta(I);
-    theta = interp1(tt.theta, theta, tt.pge2, 'linear', 'extrap');
+    [~,I] = unique(tt.theta);
+    tt.theta(I+1) = tt.theta(I+1) + 1e-12;  % offset any duplicate time points (steps)
+    theta = d(6).value(2:end-3)/2^23*pi;  % radians
+    theta = interp1(tt.theta, theta, tt.rho, 'linear', 'extrap');
 
     rf.pge2 = rho .* exp(1i*theta);
+    tt.pge2 = tt.rho;
 
     % check difference and exit if too big
     % Note that the pge2 interpreter conjugates the RF 
-    rf.seqi = interp1(tt.seq, rf.seq, tt.pge2); %, 'linear', 'extrap');
-    e = 100*max(abs(rf.seqi-conj(rf.pge2)))/max(max(abs(rf.pge2), 1e-6)); 
+    rf.seqi = interp1(tt.seq, rf.seq, tt.pge2, 'linear', 'extrap');
+    e = 100*max(abs(rf.seqi-conj(rf.pge2)))/max(max(abs(rf.pge2), 1e-6))
 
-    tol = 3;
-    if e < tol
+    if e > inf
         fprintf('RF waveform mismatch (row %d, segment %d) -- exiting\n', n, i);
+        arg.plot = true;
+        doNextSegment = false;
+    end
 
-        figure; hold on
-
-        subplot(121); hold on;
+    if arg.plot
+        subplot(5,1,4); hold off;
         title(sprintf('|RF|, segment %d', cnt));
-        plot(tt.pge2, abs(rf.pge2), 'r.-');
+        plot(tt.pge2, rho, 'r.-'); hold on;
         plot(tt.seq, abs(rf.seq), 'black');
         legend('pge2', 'Pulseq');
 
-        subplot(122); hold on;
+        subplot(5,1,5); hold off;
         title(sprintf('angle(conj(RF)), segment %d', cnt));
-        plot(tt.pge2, angle(conj(rf.pge2)), 'r.-');
+        plot(tt.theta, -d(6).value(2:end-3)/2^23*pi, 'r.-'); hold on;
         plot(tt.seq, angle(rf.seq), 'black');
         legend('pge2', 'Pulseq');
+
+        for sp = 1:5
+            subplot(5, 1, sp); 
+            ax{sp} = gca;
+            grid on;
+        end
+        linkaxes([ax{1} ax{2} ax{3} ax{4} ax{5}], 'x');  % common zoom setting (along time axis) for all tiles
+    end
+
+    if ~doNextSegment
         return;
     end
 
-    n = n2 + 1;
+    if isempty(arg.row)
+        input('Press Enter key to continue to next segment ', "s");
+        n = n2 + 1;
+    end
 end
