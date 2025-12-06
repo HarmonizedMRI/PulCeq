fa9eca42159b1c08539322a5d8fa85bfc3a5e500
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2025-12-05 21:31:12 -0500

    On branch tv7_dev 	new file:   +pge2/+utils/override.m

---
+++ matlab/+pge2/+utils/override.m
@@ -0,0 +1,9 @@
+function s = override(s, varargin)
+% override(s,'field1',value1,'field2',value2,...)
+%
+% Replace one or more fields in a struct
+
+for k = 1:2:numel(varargin)
+    s.(varargin{k}) = varargin{k+1};
+end
+
