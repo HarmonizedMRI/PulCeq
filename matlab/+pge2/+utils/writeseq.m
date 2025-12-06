08f6e9006b13942a323a4419721c6adc08bc1eea
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2025-12-06 07:53:53 -0500

    simple helper function to save typing On branch tv7_dev 	new file:   writeseq.m

---
+++ matlab/+pge2/+utils/writeseq.m
@@ -0,0 +1,20 @@
+function writeseq(seq, fov, seqName)
+% Simply calls seq.checkTiming and seq.write
+
+% --- Check sequence timing ---
+[ok, error_report]=seq.checkTiming;
+if (ok)
+    fprintf('Timing check passed successfully\n');
+else
+    fprintf('Timing check failed! Error listing follows:\n');
+    fprintf([error_report{:}]);
+    fprintf('\n');
+end
+
+% --- Output for execution ---
+seq.setDefinition('FOV', fov);
+seqName = replace(seqName, {'.seq', '.pge'}, '');
+seq.setDefinition('Name', seqName);
+seq.write([seqName '.seq']);       % Write to pulseq file
+
+
